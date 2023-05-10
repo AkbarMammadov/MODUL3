d5a7ee39b7f0fce386efc8fbf885eeee383eb7fa
ElshanNadiev13 <platin618@gmail.com>
2023-05-10 15:32:38 +0400

    modul31

---
+++ main.js
@@ -0,0 +1,12 @@
+const url = '';
+
+let currentCurr = 'RUB', exchangeCurr = 'USD';
+let currVal =1;
+
+const inpLeft = document.querySelector('.left-input');
+const inpRight = document.querySelector('.right-input');
+
+const currLeft = document.querySelector('.left-curr');
+const currRight = document.querySelector('.right-curr');
+
+const btnsLeft = document.querySelector('.left-panel').children;
